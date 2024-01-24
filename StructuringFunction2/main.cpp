b165105c241038175115108c746fb83b23624954
lsuryana <lucaselbert@gmail.com>
2024-01-24 13:33:43 +0100

    revise README.md and StructuringFunction2

---
+++ StructuringFunction2/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "distance.h"
+using namespace std;
+
+int main() {
+    cout << distance(3, 4, 5) << endl;
+    cout << distance(0.7, 2.1, 5.4) << endl;
+    return 0;   
+}
